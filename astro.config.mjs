6079b8135e76a9cc07120a57de5ae913c3d3dc42
Karen Majewicz <2367677+karenmajewicz@users.noreply.github.com>
2025-10-03 18:08:22 -0500

    more tutorial formatting

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,52 +5,57 @@ import starlightFullViewMode from 'starlight-fullview-mode';
 // import starlightLinksValidator from 'starlight-links-validator'
 import starlightBlog from 'starlight-blog';
 import react from '@astrojs/react';
+import starlightImageZoom from 'starlight-image-zoom'
+
+
+
+import icon from 'astro-icon';
 
 
 
 export default defineConfig({
-  integrations: [
-    starlight({
-      title: 'BTAA-GIN',
-      logo: {
-        src: '/src/assets/images/btaa-gin-logo.svg',
-        alt: 'BTAA-GIN',
-        replacesTitle: true,
-      },
-      // social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/geobtaa',}],
-      customCss: [
-        './src/styles/global.css',
-      ],
-      components: {
-        Footer: './src/components/FooterWithBar.astro',
-      },
-      plugins: [
-        starlightFullViewMode({
+  integrations: [starlight({
+    title: 'BTAA-GIN',
+    logo: {
+      src: '/src/assets/images/btaa-gin-logo.svg',
+      alt: 'BTAA-GIN',
+      replacesTitle: true,
+    },
+    // social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/geobtaa',}],
+    customCss: [
+      './src/styles/global.css',
+    ],
+    components: {
+      Footer: './src/components/FooterWithBar.astro',
+    },
+    plugins: [
+      starlightFullViewMode({
+      }),
+      starlightImageZoom({
+          selector: 'img[src*="/src/assets/images/"], figure img',
         }),
-        starlightBlog({
-          title: 'Blog',
-        }),
-        // starlightLinksValidator(),
-      ],
-      sidebar: [
-        {
-          label: 'About',
-          autogenerate: { directory: 'about' },
-        },
-        {
-          label: 'Projects',
-          autogenerate: { directory: 'projects' },
-        },
-        {
-          label: 'Big Ten GIS Conference',
-          link: '/conference/',
-        },
-        {
-          label: 'Document Library',
-          link: '/library/',
-        },
-      ],
-    }),
-    react(),
-  ],
+      starlightBlog({
+        title: 'Blog',
+      }),
+      // starlightLinksValidator(),
+    ],
+    sidebar: [
+      {
+        label: 'About',
+        autogenerate: { directory: 'about' },
+      },
+      {
+        label: 'Projects',
+        autogenerate: { directory: 'projects' },
+      },
+      {
+        label: 'Big Ten GIS Conference',
+        link: '/conference/',
+      },
+      {
+        label: 'Document Library',
+        link: '/library/',
+      },
+    ],
+  }), react(), icon()],
 });
