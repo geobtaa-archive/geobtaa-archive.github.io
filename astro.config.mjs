773f5fe57ae7deb9620bea0b9fe355c15acb13bc
“karenmajewicz” <majew030@umn.edu>
2025-10-05 15:32:21 -0500

    reorg attempts

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,12 +6,8 @@ import starlightFullViewMode from 'starlight-fullview-mode';
 import starlightBlog from 'starlight-blog';
 import react from '@astrojs/react';
 import starlightImageZoom from 'starlight-image-zoom'
-
-
-
+// import starlightSidebarTopics from 'starlight-sidebar-topics'
 import icon from 'astro-icon';
-
-
 
 export default defineConfig({
   integrations: [starlight({
@@ -29,37 +25,37 @@ export default defineConfig({
       Footer: './src/components/FooterWithBar.astro',
     },
     plugins: [
-      starlightFullViewMode({
-      }),
+      starlightFullViewMode({}),
       starlightImageZoom({
           selector: 'img[src*="/src/assets/images/"], figure img',
         }),
-      starlightBlog({
-        title: 'Blog',
-      }),
+      starlightBlog({title: 'Blog',}),
       // starlightLinksValidator(),
-    ],
+      ],
     sidebar: [
-      {
-        label: 'About',
-        autogenerate: { directory: 'about' },
-      },
-      {
-        label: 'Projects',
-        autogenerate: { directory: 'projects' },
-      },
-      {
-        label: 'Big Ten GIS Conference',
+
+      { label: 'Geospatial Information Network',
         items: [
-        'conference',
-        'conference/map-gallery',
+          { label: 'About', autogenerate: { directory: 'about' } },
+          { label: 'Projects', autogenerate: { directory: 'projects' } },
+          {label: 'Scholarship',
+              items: [
+                { label: 'Document Library', link: '/library/' },
+                { label: 'Publications', link: '/scholarship/publications/' },
+              ],
+          },  
+        ],
+      },
+      { label: 'Community',
+        items: [
+        { label: 'User Resources', autogenerate: { directory: 'user-resources' } },
+        { label: 'Big Ten Conference', autogenerate: { directory: 'conference' } },
         ]
-        
       },
-      {
-        label: 'Document Library',
-        link: '/library/',
-      },
-    ],
-  }), react(), icon()],
+
+    ]
+  }), // <-- THIS IS THE MISSING CLOSING PARENTHESIS
+  react(), 
+  icon()
+],
 });
