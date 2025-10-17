1d86210b39be69c8d6b5ac34fdd7088f4ee49bad
“karenmajewicz” <majew030@umn.edu>
2025-10-16 19:49:07 -0500

    remove plugin for blog

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,7 +1,7 @@
 import { defineCollection, z } from 'astro:content';
 import { docsLoader } from '@astrojs/starlight/loaders';
 import { docsSchema } from '@astrojs/starlight/schema';
-import { blogSchema } from 'starlight-blog/schema';
+import { blogSchema } from 'src/content/BlogSchema';
 import { videosSchema } from 'starlight-videos/schemas';
 
 export const collections = {
