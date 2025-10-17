5859548d4c397a10cfd685e02fc636ffe21ce84b
“karenmajewicz” <majew030@umn.edu>
2025-10-17 18:23:09 -0500

    updated filter for library

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,4 +1,4 @@
-import { defineCollection } from 'astro:content';
+import { defineCollection, z } from 'astro:content';
 import { docsLoader } from '@astrojs/starlight/loaders';
 import { docsSchema } from '@astrojs/starlight/schema';
 import { blogSchema } from 'src/content/BlogSchema';
@@ -8,16 +8,15 @@ export const collections = {
   docs: defineCollection({
     loader: docsLoader(),
     schema: docsSchema({
-      extend: (context) => blogSchema(context).and(videosSchema),
-      // extend: 
-      //   (context) =>
-      //   blogSchema(context).extend({
-      //     // Optional 4-digit year; accepts number or string and coerces to number
-      //     year: z.coerce.number().int().min(1800).max(2100).optional(),
-      //   }),
-       
-
-        
+      extend: (context) =>
+        blogSchema(context)
+          .and(videosSchema)
+          .and(
+            z.object({
+              // Optional 4-digit year; accepts number or string and coerces to number
+              year: z.coerce.number().int().min(1800).max(2100).optional(),
+            }),
+          ),
     }),
   }),
 };
